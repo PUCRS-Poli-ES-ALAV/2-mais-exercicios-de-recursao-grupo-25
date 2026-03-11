c2d8d938160c455296d3b0b37b8ed32ed8dfb9c7
Julia Fernandes <f.julia001@edu.pucrs.br>
2026-03-11 23:41:19 +0000

    commit

--- main.java
+++ main.java
@@ -3,12 +3,12 @@ public static void main (String agrs[]) throws Exception{
     int num = 5;
     int num2 = 10;
     String pal = "arara";
-    System.out.println("Fatorial = " + fatorial(num)); 
-    System.out.println("Somatorio = " + soma(num));
-    System.out.println("Fibonacci = " + fibonacci(num));
-    System.out.println("Somatorio entre k e j = " + somaInt(num, num2));
-    System.out.println("Somatorio 2 entre k e j = " + somaInt2(num, num2)); 
-    System.out.println("Se eh palindrome = " + isPal(pal)); 
+    System.out.println("Fatorial = " + fatorial(num)); // 1
+    System.out.println("Somatorio = " + soma(num)); // 2
+    System.out.println("Fibonacci = " + fibonacci(num)); // 3
+    System.out.println("Somatorio entre k e j = " + somaInt(num, num2)); // 4
+    System.out.println("Somatorio 2 entre k e j = " + somaInt2(num, num2)); // 4
+    System.out.println("Se eh palindrome = " + isPal(pal)); // 5
 
 }
 
@@ -57,3 +57,5 @@ public static boolean isPal(String s) throws Exception{
 
     return isPal(s.substring(1, s.length()-1));
 }
+
+//ex6
